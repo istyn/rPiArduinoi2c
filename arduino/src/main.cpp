c17bda3c319eccfd0e2a99852bd7e4ec5f234e57
Isaac Styles <styles@etsu.edu>
2017-01-03 16:36:42 -0500

    initial commit

---
+++ arduino/src/main.cpp
@@ -0,0 +1,65 @@
+#include <Wire.h>
+#include <Arduino.h>
+#define SLAVE_ADDRESS 0x04
+int number = 0;
+int state = 0;
+
+// callback for received data
+void receiveData(int byteCount){
+
+  while(Wire.available()) {
+    number = Wire.read();
+    Serial.print("data received:");
+    Serial.println(number);
+
+    if (number == 1){
+
+      if (state == 0){
+        digitalWrite(13, HIGH); // set the LED on
+        state = 1;
+      }
+      else
+      {
+        digitalWrite(13, LOW); // set the LED off
+        state = 0;
+      }
+    }
+  }
+}
+
+double Thermister(int RawADC) {  //Function to perform the fancy math of the Steinhart-Hart equation
+ double Temp;
+ Temp = log(((10240000/RawADC) - 10000));
+ Temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * Temp * Temp ))* Temp );
+ Temp = Temp - 273.15;              // Convert Kelvin to Celsius
+ Temp = (Temp * 9.0)/ 5.0 + 32.0; // Celsius to Fahrenheit - comment out this line if you need Celsius
+ return Temp;
+}
+
+// callback for sending data
+void sendData(){
+  int val;                //Create an integer variable
+  double temp;            //Variable to hold a temperature value
+  val=analogRead(0);      //Read the analog port 0 and store the value in val
+  temp=Thermister(val);   //Runs the fancy math on the raw analog value
+  Serial.println(temp);   //Print the value to the serial port
+Wire.write((int)temp);
+//Serial.println(number);
+}
+
+void setup() {
+pinMode(13, OUTPUT);
+Serial.begin(9600); // start serial for output
+// initialize i2c as slave
+Wire.begin(SLAVE_ADDRESS);
+
+// define callbacks for i2c communication
+//Wire.onReceive(receiveData);
+Wire.onRequest(sendData);
+
+Serial.println("Ready");
+}
+
+void loop() {
+delay(1000);
+}
