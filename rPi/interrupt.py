9dc867944e78ea36312ee1e4909221d754511474
Isaac Styles <styles@etsu.edu>
2017-01-04 18:23:41 -0500

    switch now cycles the state of the status LED

--- rPi/interrupt.py
+++ rPi/interrupt.py
@@ -9,13 +9,20 @@ GPIO.setmode(GPIO.BCM)
 # this enables us to demonstrate both rising and falling edge detection  
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
 GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
-GPIO.setup(21, GPIO.OUT)  
+GPIO.setup(21, GPIO.OUT)
+GPIO.output(21, False)
+state = 0
 # now we'll define the threaded callback function  
 # this will run in another thread when our event is detected  
 def my_callback(channel):  
     print "Rising edge detected on port 24 - even though, in the main thread,"  
     print "we are still waiting for a falling edge - how cool?\n"
-    GPIO.output(21, True)
+    if (state = 0):
+        GPIO.output(21, True)
+        state = 1
+    else
+        GPIO.output(21, False)
+        state = 0
   
 print "Make sure you have a button connected so that when pressed"  
 print "it will connect GPIO port 23 (pin 16) to GND (pin 6)\n"  
@@ -28,7 +35,7 @@ raw_input("Press Enter when ready\n>")
 # else is happening in the program, the function "my_callback" will be run  
 # It will happen even while the program is waiting for  
 # a falling edge on the other button.  
-GPIO.add_event_detect(24, GPIO.RISING, callback=my_callback, bouncetime=200)  
+GPIO.add_event_detect(24, GPIO.FALLING, callback=my_callback, bouncetime=200)  
   
 try:  
     print "Waiting for falling edge on port 23"  
